3a46cca9bbd6df5a8dfbdceaac3c4b2eb7d467b7
Brutus5000 <Brutus5000@gmx.net>
2021-10-20 23:25:14 +0200

    Fix contract id not incrementing

--- src/domain/contract_store.rs
+++ src/domain/contract_store.rs
@@ -2,14 +2,14 @@ use crate::domain::contract::Contract;
 use std::cell::RefCell;
 
 pub struct ContractStore {
-    next_index: u32,
+    next_index: RefCell<u32>,
     contracts: RefCell<Vec<Contract>>,
 }
 
 impl ContractStore {
     pub fn new() -> Self {
         ContractStore {
-            next_index: 1,
+            next_index: RefCell::new(1),
             contracts: RefCell::new(vec![]),
         }
     }
@@ -19,12 +19,14 @@ impl ContractStore {
     }
 
     pub fn create(&self, name: &str) {
+        let next_index = *self.next_index.borrow();
         let new_contract = Contract {
-            id: self.next_index,
+            id: next_index,
             name: String::from(name),
         };
 
         self.contracts.borrow_mut().push(new_contract);
+        self.next_index.replace(next_index + 1);
     }
 
     pub fn delete(&self, id: u32) {
